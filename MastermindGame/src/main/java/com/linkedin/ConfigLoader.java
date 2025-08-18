bab2ffbe1ca1c5b9afbd920659429af6a8661efa
Akbar <akbarsaidakramov@gmail.com>
2025-08-18 12:58:51 -0700

    adding config file with random api key, and adding config reader class to read the key from the file

---
+++ MastermindGame/src/main/java/com/linkedin/ConfigLoader.java
@@ -0,0 +1,24 @@
+package com.linkedin;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigLoader {
+    private static Properties properties = new Properties();
+
+    static { // open data as a stream and stores it in the memory
+        try (InputStream input = ConfigLoader.class.getClassLoader().getResourceAsStream("config.properties")) {
+            if (input == null) {
+                throw  new RuntimeException("config.properties not found in the resources folder");
+            }
+            properties.load(input);
+        } catch (IOException e) {
+            throw new RuntimeException("Error loading config.properties", e);
+        }
+    }
+
+    public static String get(String key){
+        return properties.getProperty(key);
+    }
+}
